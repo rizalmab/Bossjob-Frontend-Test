0643467775db78c2b44aa7411a21a03211046e31
rizalmab <rizalmahadiab@gmail.com>
2022-03-14 13:58:51 +0800

    Added basic search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Header from './shared/components/Header'
+import Header from "./shared/components/Header";
+import Search from "./assets/images/search.png";
 
 class App extends Component {
   render() {
@@ -9,10 +10,24 @@ class App extends Component {
       <div className="App">
         <div className="App-Wrapper">
           <Header />
-          <div style={{padding: '10px 20px'}}>
-            <p>
-              Please show a list of jobs as per design
+          {/* <div style={{ padding: "10px 20px" }}>
+            <p>Please show a list of jobs as per design</p>
+          </div> */}
+          <div>
+            <img
+              alt="search icon"
+              src={Search}
+              style={{ height: "20px", width: "20px", display: "inline-block" }}
+            />
+            <p style={{ display: "inline-block" }}>
+              Search for job title or company name
             </p>
+          </div>
+          <div>
+            <p>Filter results</p>
+          </div>
+          <div>
+            <p>Jobs section</p>
           </div>
         </div>
       </div>
